3e12f36105f75c487fd698149790d3a07d1afb1e
Riccardo Balbo <riccardo@forkforge.net>
2020-07-01 13:50:00 +0200

    Add web ui

---
+++ webui/workerOffline.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(185, 32);  
+  }
+  
+  
+  function draw() {
+    background(255,0,0);
+    textSize(20);
+    text('Worker: Offline', 20, 22);
+  }
---
+++ webui/workerOnline.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(185, 32);  
+  }
+  
+  
+  function draw() {
+    background(167, 209, 41);
+    textSize(20);
+    text('Worker: Online', 20, 22);
+  }
